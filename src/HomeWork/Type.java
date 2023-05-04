0d351ed9de6c52c39e205d39f3ab0008b33aca4b
Mikhailqwqwqwqwqwqwqwqwqwqwqwqwq <mishavitebsk96@gmail.com>
2023-05-04 16:02:07 +0300

    add my new code

---
+++ src/HomeWork/Type.java
@@ -0,0 +1,13 @@
+package HomeWork;
+
+public enum Type {
+    public Worker(String name, int age, WorkerType workerType) {
+        this.name = name;
+        this.age = age;
+        this.workerType = workerType;
+    }
+
+    public void setWorkerType(WorkerType workerType) {
+        this.workerType = workerType;
+    }
+}
