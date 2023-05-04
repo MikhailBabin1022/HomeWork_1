7074796f2808bf092c9e8d54f97128ede842dd38
Mikhailqwqwqwqwqwqwqwqwqwqwqwqwq <mishavitebsk96@gmail.com>
2023-05-04 22:32:53 +0300

    add my new code

---
+++ src/HomeWork/Security.java
@@ -0,0 +1,38 @@
+package HomeWork;
+
+public class Security {
+    public SecurityGuard(String name, int age, String idNumber, String company) {
+        this.name = name;
+        this.age = age;
+        this.idNumber = idNumber;
+        this.company = company;
+    }
+
+    public void checkId(String id) {
+
+    }
+
+    public void patrol() {
+
+    }
+
+    public void monitor() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getIdNumber() {
+        return idNumber;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+}
