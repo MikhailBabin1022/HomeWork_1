fc61a2c73c30ad9ff37ead50b125b271a4940249
Mikhailqwqwqwqwqwqwqwqwqwqwqwqwq <mishavitebsk96@gmail.com>
2023-05-04 16:06:18 +0300

    add my new code

---
+++ src/HomeWork/Dvornik.java
@@ -0,0 +1,12 @@
+package HomeWork;
+
+public class Dvornik {
+    public Dvornik(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void clean() {
+        System.out.println("Дворник " + name + " убирает на улице");
+    }
+}
